9a6a1444181091f00bccc9c292c49ce7949d16c8
502296 <ali.kathem.edu@gmail.com>
2026-06-01 13:41:28 -0400

    Change background opacity from 30% to 45%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -646,7 +646,7 @@ const SignupFacilityPage = () => (
   backgroundRepeat: "no-repeat",
 }}
     >
-      <div className="absolute inset-0 bg-black/30" />
+      <div className="absolute inset-0 bg-black/45" />
       <Header />
 
       <div className="relative z-10 max-w-5xl mx-auto text-center pt-24">
