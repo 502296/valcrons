dcd34b1dc77c3791f7822ee49cac159384af78fa
502296 <ali.kathem.edu@gmail.com>
2026-05-31 21:53:22 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -646,7 +646,7 @@ const SignupFacilityPage = () => (
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="absolute inset-0 bg-black/60" />
+      <div className="absolute inset-0 bg-black/30" />
       <Header />
 
       <div className="relative z-10 max-w-5xl mx-auto text-center">
