ab34e060b953b84932f5d9fa1045d76076aa16f4
502296 <ali.kathem.edu@gmail.com>
2026-06-03 21:24:36 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
 import Hero from "@/components/home/Hero";
 import FeatureCards from "@/components/home/FeatureCards";
 import HowItWorks from "@/components/home/HowItWorks";
@@ -6,12 +8,16 @@ import Industries from "@/components/home/Industries";
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Hero />
-      <FeatureCards />
-      <HowItWorks />
-      <Industries />
-    </main>
+    <>
+      <main>
+        <Header />
+        <Hero />
+        <FeatureCards />
+        <HowItWorks />
+        <Industries />
+      </main>
+
+      <Footer />
+    </>
   );
 }
