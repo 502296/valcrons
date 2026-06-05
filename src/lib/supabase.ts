d4adc1637465ca2c18271f47ece9c75776c43baa
502296 <ali.kathem.edu@gmail.com>
2026-06-04 21:35:02 -0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,17 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-  .trim()
-  .replace(/`/g, "")
-  .replace(/"/g, "")
-  .replace(/'/g, "")
-  .replace(/\/$/, "");
+const supabaseUrl = "https://gethyhjzqyblovtoodhw.supabase.co";
 
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  .trim()
-  .replace(/`/g, "")
-  .replace(/"/g, "")
-  .replace(/'/g, "");
+const supabaseAnonKey = "PASTE_YOUR_SUPABASE_ANON_PUBLIC_KEY_HERE";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
