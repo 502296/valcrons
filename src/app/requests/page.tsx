57d06c250cd48374c59136c6620c9e126585a760
502296 <ali.kathem.edu@gmail.com>
2026-06-09 19:19:46 -0400

    Update page.tsx

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import BackButton from "@/components/layout/BackButton";
