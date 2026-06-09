e957c83d8b8a1830eaa12a74b79ba99dbbf3e80a
502296 <ali.kathem.edu@gmail.com>
2026-06-09 18:54:08 -0400

    Update page.tsx

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
+import Link from "next/link";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import BackButton from "@/components/layout/BackButton";
